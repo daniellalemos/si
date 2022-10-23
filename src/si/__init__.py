534ac766018bcdb46a172df244621bda0a90ccab
daniellalemos <105716000+daniellalemos@users.noreply.github.com>
2022-10-23 15:42:41 +0100

    Fix minor issue

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -4,3 +4,8 @@ __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Daniela Lemos"
 __email__ = "pg45469@alunos.uminho.p"
+
+
+# -----------------------------------------------------------------------------------
+# This package is heavily inspired and adapted from https://github.com/vmspereira/si
+# -----------------------------------------------------------------------------------
